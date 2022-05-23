1d6245aa9aeba38de8277fe8c9509d7504a94879
junghoonkim <junghoon@rescale.com>
2022-05-23 20:07:13 +0900

    Add new file

---
+++ ziparchive_inputs.py
@@ -0,0 +1,22 @@
+import os
+import zipfile
+
+CWD = os.getcwd()
+Folder_Inputs = CWD + '\\' + 'Input Files'
+# The folder name "Input Files" could be changed as your
+os.chdir(Folder_Inputs)
+
+
+List_inputfiles = []
+
+for (root, directories, files) in os.walk(Folder_Inputs):
+    for file in files:
+        List_inputfiles.append(file)
+
+print(List_inputfiles)
+
+with zipfile.ZipFile("inputfiles.zip", "w") as zip_inputs:
+    for file in List_inputfiles:
+        zip_inputs.write(file)
+    zip_inputs.close()
+
